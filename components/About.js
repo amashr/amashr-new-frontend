45bfb49e25819f7b12c652584ba91e7498436219
FrugenceFide <frugencefidel@gmail.com>
2019-10-04 16:26:20 +0300

    About content

--- components/About.js
+++ components/About.js
@@ -1,12 +1,39 @@
 import styled from 'styled-components';
 
 const AboutStyled = styled.section`
-  background: purple;
+  padding: 13rem 0;
 `;
 
 const About = () => (
   <AboutStyled id="about">
-    <h2>About</h2>
+    <div>
+      <div className="about-item">
+        <div className="icon">
+          <img src="static/img/gear.svg" alt="Setting" />
+        </div>
+        <h2>Bootstrap</h2>
+        <p>
+          Share processes and data secure lona need to know basis Our team
+          assured your web site is always safe.
+        </p>
+      </div>
+    </div>
+    <div>
+      <div>
+        <img src="static/img/about.svg" alt="About" />
+      </div>
+      <div>
+        <h3>Innovative and Collaborative Human Capital Solutions</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        <a href="#">Download</a>
+      </div>
+    </div>
   </AboutStyled>
 );
 
