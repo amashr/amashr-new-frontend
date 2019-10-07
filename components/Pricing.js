fe8826f65c7202a32435e8810ecf55cc59454db8
FrugenceFide <frugencefidel@gmail.com>
2019-10-07 17:54:33 +0300

    Add pricing compoent

---
+++ components/Pricing.js
@@ -0,0 +1,7 @@
+const Pricing = () => (
+  <section>
+    <h3>Pricing</h3>
+  </section>
+);
+
+export default Pricing;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Main from '../components/Main';
 import About from '../components/About';
 import Services from '../components/Services';
+import Pricing from '../components/Pricing';
 
 const Home = () => (
   <>
     <Main />
     <About />
     <Services />
+    <Pricing />
   </>
 );
 
